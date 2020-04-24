fc8c669f422a6004dcb4b34203488baf58a0fc43
FullOfOrange <foorgdev@gmail.com>
2020-04-24 12:39:40 +0900

    feat: 이미지 옵티마이저

--- pkg/optimizer/optimizer.go
+++ pkg/optimizer/optimizer.go
@@ -2,31 +2,62 @@ package optimizer
 
 import (
 	"bytes"
-	"fmt"
-	"image/jpeg"
+	"image"
 	"image/png"
-	"net/http"
+	_ "image/jpeg"
+	_ "image/gif"
+
+	"github.com/disintegration/imaging"
 )
 
-// ToPng converts an image to png
-func ToPng(imageBytes []byte) ([]byte, error) {
-	contentType := http.DetectContentType(imageBytes)
+type Image struct {
+	src *image.Image
+	conf *image.Config
+}
 
-	switch contentType {
-	case "image/png":
-	case "image/jpeg":
-		img, err := jpeg.Decode(bytes.NewReader(imageBytes))
-		if err != nil {
-			return nil, err
-		}
+var pngEncoder = png.Encoder{CompressionLevel: -3}
 
-		buf := new(bytes.Buffer)
-		if err := png.Encode(buf, img); err != nil {
-			return nil, err
-		}
+func (m *Image) Resize(width int, height int) *Image {
+	resize_image := imaging.Resize(*m.src, width, height, imaging.Lanczos);
 
-		return buf.Bytes(), nil
+	*m.src = resize_image
+	return m
+}
+
+func (m *Image) ResizeWithHeight (height int) *Image {
+	p := height / m.conf.Height
+	m.Resize(m.conf.Width * p, height)
+	return m
+}
+
+func (m *Image) ResizeWithWidth (width int) *Image {
+	p := width / m.conf.Width
+	m.Resize(width, m.conf.Height * p)
+	return m
+}
+
+func (m *Image) ImageToPng() ([]byte, error) {
+	buf := new(bytes.Buffer);
+
+	if err := pngEncoder.Encode(buf, *m.src); err != nil {
+		return nil, err;
+	}
+	return buf.Bytes(), nil
+}
+
+func ByteToImage(byte []byte) (*Image, error) {
+	//contentType := http.DetectContentType(byte)
+
+	reader := bytes.NewReader(byte)
+	img, _, err := image.Decode(reader);
+	if err != nil {
+		return nil, err
+	}
+	reader.Reset(byte);
+	config, _, err := image.DecodeConfig(reader)
+	if err != nil {
+		return nil, err
 	}
 
-	return nil, fmt.Errorf("unable to convert %#v to png", contentType)
+	return &Image{&img, &config}, nil;
 }
