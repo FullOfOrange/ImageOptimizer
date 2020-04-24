bb95df92466f3f26862f9d71d4e43a5780448c69
FullOfOrange <foorgdev@gmail.com>
2020-04-24 16:51:27 +0900

    chore: 업로드 예시를 추가함

--- main.go
+++ main.go
@@ -1,28 +1,4 @@
 package main
 
-import (
-	"log"
-
-	"github.com/FullOfOrange/Devlog-Image/pkg/optimizer"
-	"github.com/FullOfOrange/Devlog-Image/pkg/uploader"
-)
-
 func main() {
-	image_file, err := uploader.GetImage("image")
-	if err != nil {
-		log.Fatal("1we2", err)
-	}
-
-	img, err := optimizer.ByteToImage(image_file);
-	if err != nil {
-		log.Fatal("asdf", err)
-	}
-
-	byte, err := img.ResizeWithHeight(300).ImageToPng();
-	if err != nil {
-
-		log.Fatal("ㅁㄴㅇㄹ", err)
-	}
-
-	uploader.SaveImage(byte)
 }
