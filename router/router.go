ae675b4350782a08be72c3653497e52000147425
FullOfOrange <foorgdev@gmail.com>
2020-04-25 21:07:41 +0900

    feat: 이미지 다운로드 핸들 작성

--- router/router.go
+++ router/router.go
@@ -1,54 +1,84 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 
-	"github.com/FullOfOrange/Devlog-Image/pkg/optimizer"
-	"github.com/FullOfOrange/Devlog-Image/pkg/uploader"
+	"github.com/FullOfOrange/ImageOptimizer/pkg/optimizer"
+	"github.com/FullOfOrange/ImageOptimizer/pkg/uploader"
 )
 
 func InitRouter() *http.ServeMux{
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", imageDownloadHandler)
+	mux.HandleFunc("/", imageHandler)
 	return mux
 }
 
-func imageDownloadHandler(w http.ResponseWriter, r *http.Request){
-	if r.Method != http.MethodGet {
+func imageHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		url := r.URL
+		filename := strings.TrimLeft(url.Path, "/");
+		width, height := getImageOptSize(url.Query())
+		str := filename +"?"+getImageOptQueryString(width, height)
+
+		imagebyte, err := uploader.GetImage(str)
+		if err != nil {
+			imagebyte, err = uploader.GetImage(filename)
+			if err != nil {
+				w.WriteHeader(404)
+				return
+			}
+
+			image, err := optimizer.ByteToImage(imagebyte);
+			if err != nil {
+				w.WriteHeader(500)
+				return
+			}
+
+			imagebyte, err = image.ResizeWithWidth(width).ImageToJPEGByte()
+			if err != nil {
+				w.WriteHeader(500)
+				return
+			}
+
+			uploader.SaveImage(imagebyte, str)
+		}
+		w.Header().Add("Content-Type", "image/jpeg")
+		w.WriteHeader(200);
+		if n, err := w.Write(imagebyte); err != nil || n != len(imagebyte) {
+			w.WriteHeader(500)
+			return
+		}
+	} else if r.Method == http.MethodPost {
+
+	} else {
 		w.WriteHeader(404)
 		return
 	}
+}
 
-	url := r.URL
-	filename := strings.TrimLeft(url.Path,"/");
-
-	query := url.Query()
-
-	imagebyte, err := uploader.GetImage(filename)
+func getImageOptSize(query url.Values) (width int, height int) {
+	var err error
+	width, err = strconv.Atoi(query.Get("width"));
 	if err != nil {
-		w.WriteHeader(404)
-		return
+		width = 0;
 	}
+	height, err = strconv.Atoi(query.Get("height"));
+	if err != nil {
+		height = 0;
+	}
+	return
+}
 
-	if width, err := strconv.Atoi(query.Get("width")); err == nil {
-		image, err := optimizer.ByteToImage(imagebyte);
-		if err != nil {
-			w.WriteHeader(500)
-			return
-		}
-		imagebyte, err = image.ResizeWithWidth(width).ImageToJPEGByte()
-		if err != nil {
-			w.WriteHeader(500)
-			return
-		}
+func getImageOptQueryString(width int, height int) (result string){
+	if width != 0 {
+		result += fmt.Sprintf("width=%d", width)
 	}
-
-	w.Header().Add("Content-Type","image/jpeg")
-	w.WriteHeader(200);
-	if n, err := w.Write(imagebyte); err != nil || n != len(imagebyte){
-		w.WriteHeader(500)
-		return
+	if height != 0 {
+		result += fmt.Sprintf("height=%d", height)
 	}
+	return
 }
